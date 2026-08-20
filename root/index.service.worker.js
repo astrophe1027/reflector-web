db39fba720015ed28086b9ed8e5d550661874b88
astrophe1027 <junghyunjin20@gmail.com>
2026-08-20 19:41:56 +0900

    .

--- root/index.service.worker.js
+++ root/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787055337|212565112';
+const CACHE_VERSION = '1787222482|5636507510';
 /** @type {string} */
 const CACHE_PREFIX = 'Reflector-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
